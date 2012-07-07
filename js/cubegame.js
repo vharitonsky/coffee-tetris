2781ca682aaa022d93d0ac9e27edd202af86a2af
Vitaly Haritonsky <vharitonsky@gmail.com>
2012-07-07 10:33:05 +0300

    init commit

---
+++ js/cubegame.js
@@ -0,0 +1,492 @@
+(function() {
+  var Cell, Cross, Cube, Figure, Stick, ZShape, check_rows, clear_cell, direction_from_event, draw_cell, generate_brick, get_cell, handle_click_event, handle_key_event, init, random_cells, random_color, self, set_cell,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  this.canvas = '';
+
+  this.cell_width = 40;
+
+  this.cell_height = 40;
+
+  this.height = 600;
+
+  this.width = 800;
+
+  self = this;
+
+  $(function() {
+    self.canvas = document.getElementById('canvas').getContext('2d');
+    return init();
+  });
+
+  init = function() {
+    self.cells = {};
+    self.shapes = [];
+    /*
+        self.shapes=[new Cross(80,80),
+                    new Cube(200,80),
+                    new Cross(320,80),
+                    new Stick(80,240)]
+    */
+    $('#canvas').click(function(event) {
+      return event.preventDefault();
+    });
+    $('body').keydown(handle_key_event);
+    $('body').click(handle_click_event);
+    return generate_brick();
+  };
+
+  check_rows = function() {
+    return false;
+  };
+
+  generate_brick = function() {
+    var shape, shape_class, shape_classes;
+    shape_classes = [Cross, Cube, Stick, ZShape];
+    shape_class = shape_classes[Math.floor(Math.random() * shape_classes.length)];
+    shape = new shape_class(400, 40);
+    self.selected_shape = shape;
+    return shape.fall();
+  };
+
+  clear_cell = function(x, y) {
+    return delete self.cells[x][y];
+  };
+
+  get_cell = function(x, y) {
+    if (!self.cells[x]) return;
+    return self.cells[x][y];
+  };
+
+  set_cell = function(x, y, cell) {
+    if (!self.cells[x]) self.cells[x] = {};
+    return self.cells[x][y] = cell;
+  };
+
+  handle_key_event = function(event) {
+    var direction, falling, rotation;
+    if (!self.selected_shape) return;
+    direction = direction_from_event(event);
+    if (direction) {
+      selected_shape.move(direction);
+      return;
+    }
+    rotation = event.keyCode === 32;
+    if (rotation) {
+      selected_shape.rotate();
+      return;
+    }
+    falling = event.keyCode === 70;
+    if (falling) selected_shape.fall();
+  };
+
+  handle_click_event = function(event) {
+    var shape, _i, _len, _ref;
+    event.preventDefault();
+    _ref = self.shapes;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      shape = _ref[_i];
+      if (shape.point_in_shape(event.offsetX, event.offsetY)) {
+        self.selected_shape = shape;
+        return;
+      }
+    }
+  };
+
+  direction_from_event = function(event) {
+    var key_map;
+    key_map = {
+      39: 'right',
+      37: 'left',
+      40: 'down'
+    };
+    return key_map[event.keyCode];
+  };
+
+  random_cells = function() {
+    var i, j, _results;
+    _results = [];
+    for (i = 0; i <= 20; i++) {
+      _results.push((function() {
+        var _results2;
+        _results2 = [];
+        for (j = 0; j <= 15; j++) {
+          _results2.push(draw_cell(i + j, i * self.cell_width, j * self.cell_height));
+        }
+        return _results2;
+      })());
+    }
+    return _results;
+  };
+
+  random_color = function() {
+    var blue, green, red;
+    red = Math.ceil(Math.random() * 255);
+    green = Math.ceil(Math.random() * 255);
+    blue = Math.ceil(Math.random() * 255);
+    if (red > 255) red = 255;
+    if (green > 255) green = 255;
+    if (blue > 255) blue = 255;
+    return "rgb(" + red + "," + green + "," + blue + ")";
+  };
+
+  draw_cell = function(left_x, top_y) {
+    return self.canvas.fillRect(left_x, top_y, self.cell_width, self.cell_height);
+  };
+
+  Figure = (function() {
+
+    Figure.prototype.color = 'rgb(255,255,255)';
+
+    Figure.prototype.cells = [];
+
+    Figure.width = 0;
+
+    Figure.height = 0;
+
+    Figure.x = 0;
+
+    Figure.y = 0;
+
+    function Figure(x, y) {
+      this.fall = __bind(this.fall, this);
+      this.detect_collision = __bind(this.detect_collision, this);      this.color = random_color();
+      this.cells = [];
+      if ((x != null) && (y != null)) this.draw(x, y);
+    }
+
+    Figure.prototype.point_in_shape = function(x, y) {
+      return x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height;
+    };
+
+    Figure.prototype.rotate = function() {
+      return false;
+    };
+
+    Figure.prototype.draw = function(left_x, top_y) {
+      this.x = left_x;
+      this.y = top_y;
+      return self.canvas.fillStyle = this.color;
+    };
+
+    Figure.prototype.outline = function() {
+      var stroke_style;
+      stroke_style = self.canvas.strokeStyle;
+      self.canvas.strokeStyle = 'rgb(0,0,0)';
+      self.canvas.strokeRect(this.x, this.y, this.width, this.height);
+      return self.canvas.strokeStyle = stroke_style;
+    };
+
+    Figure.prototype.remove_outline = function() {
+      var stroke_style;
+      stroke_style = self.canvas.strokeStyle;
+      self.canvas.strokeStyle = 'rgb(255,255,255)';
+      self.canvas.strokeRect(this.x, this.y, this.width, this.height);
+      return self.canvas.strokeStyle = stroke_style;
+    };
+
+    Figure.prototype.clear = function() {
+      var cell, _i, _len, _ref, _results;
+      _ref = this.cells;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        cell = _ref[_i];
+        _results.push(cell.clear());
+      }
+      return _results;
+    };
+
+    Figure.prototype.move = function(direction) {
+      if (this.detect_collision(direction)) return false;
+      this.clear();
+      if (direction === 'up') this.y -= self.cell_height;
+      if (direction === 'down') this.y += self.cell_height;
+      if (direction === 'left') this.x -= self.cell_width;
+      if (direction === 'right') this.x += self.cell_width;
+      this.draw(this.x, this.y);
+      return true;
+    };
+
+    Figure.prototype.detect_collision = function(direction) {
+      var cell, _i, _j, _k, _l, _len, _len2, _len3, _len4, _ref, _ref2, _ref3, _ref4;
+      if (direction === 'up') {
+        _ref = this.upper_cells();
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          cell = _ref[_i];
+          if (get_cell(cell.x, cell.y - self.cell_height)) return true;
+        }
+      }
+      if (direction === 'down') {
+        _ref2 = this.bottom_cells();
+        for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+          cell = _ref2[_j];
+          if (get_cell(cell.x, cell.y + self.cell_height) || this.bottom_y() + self.cell_height > self.height) {
+            return true;
+          }
+        }
+      }
+      if (direction === 'left') {
+        _ref3 = this.left_cells();
+        for (_k = 0, _len3 = _ref3.length; _k < _len3; _k++) {
+          cell = _ref3[_k];
+          if (get_cell(cell.x - self.cell_width, cell.y)) return true;
+        }
+      }
+      if (direction === 'right') {
+        _ref4 = this.right_cells();
+        for (_l = 0, _len4 = _ref4.length; _l < _len4; _l++) {
+          cell = _ref4[_l];
+          if (get_cell(cell.x + self.cell_width, cell.y)) return true;
+        }
+      }
+      return false;
+    };
+
+    Figure.prototype.bottom_y = function() {
+      return this.y + this.height;
+    };
+
+    Figure.prototype.fall = function() {
+      if (this.move('down')) {
+        return setTimeout(this.fall, 1000);
+      } else {
+        check_rows();
+        return generate_brick();
+      }
+    };
+
+    return Figure;
+
+  })();
+
+  Cell = (function(_super) {
+
+    __extends(Cell, _super);
+
+    Cell.prototype.width = self.cell_width;
+
+    Cell.prototype.height = self.cell_height;
+
+    function Cell(x, y, color) {
+      this.color = color;
+      if ((x != null) && (y != null)) this.draw(x, y);
+    }
+
+    Cell.prototype.draw = function(left_x, top_y) {
+      Cross.__super__.draw.call(this, left_x, top_y);
+      draw_cell(left_x + self.cell_width, top_y);
+      return set_cell(left_x, top_y, this);
+    };
+
+    Cell.prototype.clear = function() {
+      var original_color;
+      original_color = this.color;
+      this.color = 'rgb(255,255,255)';
+      this.draw(this.x, this.y);
+      this.color = original_color;
+      return clear_cell(this.x, this.y);
+    };
+
+    return Cell;
+
+  })(Figure);
+
+  Cross = (function(_super) {
+
+    __extends(Cross, _super);
+
+    function Cross() {
+      Cross.__super__.constructor.apply(this, arguments);
+    }
+
+    Cross.prototype.width = 3 * self.cell_width;
+
+    Cross.prototype.height = 3 * self.cell_height;
+
+    Cross.prototype.upper_cells = function() {
+      return [this.cells[0], this.cells[1], this.cells[3]];
+    };
+
+    Cross.prototype.bottom_cells = function() {
+      return [this.cells[1], this.cells[4], this.cells[3]];
+    };
+
+    Cross.prototype.left_cells = function() {
+      return [this.cells[0], this.cells[1], this.cells[4]];
+    };
+
+    Cross.prototype.right_cells = function() {
+      return [this.cells[0], this.cells[3], this.cells[4]];
+    };
+
+    Cross.prototype.draw = function(left_x, top_y) {
+      Cross.__super__.draw.call(this, left_x, top_y);
+      return this.cells = [new Cell(left_x + self.cell_width, top_y, this.color), new Cell(left_x, top_y + self.cell_height, this.color), new Cell(left_x + self.cell_width, top_y + self.cell_height, this.color), new Cell(left_x + 2 * self.cell_width, top_y + cell_height, this.color), new Cell(left_x + self.cell_width, top_y + 2 * cell_height, this.color)];
+    };
+
+    return Cross;
+
+  })(Figure);
+
+  Cube = (function(_super) {
+
+    __extends(Cube, _super);
+
+    function Cube() {
+      Cube.__super__.constructor.apply(this, arguments);
+    }
+
+    Cube.prototype.width = 2 * self.cell_width;
+
+    Cube.prototype.height = 2 * self.cell_height;
+
+    Cube.prototype.upper_cells = function() {
+      return [this.cells[0], this.cells[1]];
+    };
+
+    Cube.prototype.bottom_cells = function() {
+      return [this.cells[2], this.cells[3]];
+    };
+
+    Cube.prototype.left_cells = function() {
+      return [this.cells[0], this.cells[2]];
+    };
+
+    Cube.prototype.right_cells = function() {
+      return [this.cells[1], this.cells[3]];
+    };
+
+    Cube.prototype.draw = function(left_x, top_y) {
+      Cube.__super__.draw.call(this, left_x, top_y);
+      return this.cells = [new Cell(left_x, top_y, this.color), new Cell(left_x + self.cell_width, top_y, this.color), new Cell(left_x, top_y + self.cell_height, this.color), new Cell(left_x + self.cell_width, top_y + self.cell_height, this.color)];
+    };
+
+    return Cube;
+
+  })(Figure);
+
+  Stick = (function(_super) {
+
+    __extends(Stick, _super);
+
+    function Stick() {
+      Stick.__super__.constructor.apply(this, arguments);
+    }
+
+    Stick.prototype.width = 4 * self.cell_width;
+
+    Stick.prototype.height = self.cell_height;
+
+    Stick.prototype.upper_cells = function() {
+      if (this.width > this.height) {
+        return this.cells;
+      } else {
+        return [this.cells[0]];
+      }
+    };
+
+    Stick.prototype.bottom_cells = function() {
+      if (this.width > this.height) {
+        return this.cells;
+      } else {
+        return [this.cells[3]];
+      }
+    };
+
+    Stick.prototype.left_cells = function() {
+      if (this.width > this.height) {
+        return [this.cells[0]];
+      } else {
+        return this.cells;
+      }
+    };
+
+    Stick.prototype.right_cells = function() {
+      if (this.width > this.height) {
+        return [this.cells[3]];
+      } else {
+        return this.cells;
+      }
+    };
+
+    Stick.prototype.rotate = function() {
+      var height, width;
+      this.clear();
+      width = this.width;
+      height = this.height;
+      this.height = width;
+      this.width = height;
+      return this.draw(this.x, this.y);
+    };
+
+    Stick.prototype.draw = function(left_x, top_y) {
+      var i, inc, result, _results;
+      Stick.__super__.draw.call(this, left_x, top_y);
+      if (this.width > this.height) {
+        inc = function(x, y) {
+          return [x + self.cell_width, y];
+        };
+      } else {
+        inc = function(x, y) {
+          return [x, y + self.cell_height];
+        };
+      }
+      _results = [];
+      for (i = 0; i <= 3; i++) {
+        if (!this.cells[i]) this.cells[i] = new Cell(null, null, this.color);
+        this.cells[i].draw(left_x, top_y, this.color);
+        result = inc(left_x, top_y);
+        left_x = result[0];
+        _results.push(top_y = result[1]);
+      }
+      return _results;
+    };
+
+    return Stick;
+
+  })(Figure);
+
+  ZShape = (function(_super) {
+
+    __extends(ZShape, _super);
+
+    function ZShape() {
+      ZShape.__super__.constructor.apply(this, arguments);
+    }
+
+    ZShape.prototype.width = 2 * self.cell_width;
+
+    ZShape.prototype.height = 2 * self.cell_height;
+
+    ZShape.prototype.rotated = false;
+
+    ZShape.prototype.upper_cells = function() {
+      return [this.cells[0], this.cells[1], this.cells[3]];
+    };
+
+    ZShape.prototype.bottom_cells = function() {
+      return [this.cells[0], this.cells[2], this.cells[3]];
+    };
+
+    ZShape.prototype.left_cells = function() {
+      return [this.cells[0], this.cells[2]];
+    };
+
+    ZShape.prototype.right_cells = function() {
+      return [this.cells[1], this.cells[3]];
+    };
+
+    ZShape.prototype.rotate = function() {};
+
+    ZShape.prototype.draw = function(left_x, top_y) {
+      ZShape.__super__.draw.call(this, left_x, top_y);
+      return this.cells = [new Cell(left_x, top_y, this.color), new Cell(left_x + self.cell_width, top_y, this.color), new Cell(left_x + self.cell_width, top_y + self.cell_height, this.color), new Cell(left_x + 2 * self.cell_width, top_y + self.cell_height, this.color)];
+    };
+
+    return ZShape;
+
+  })(Figure);
+
+}).call(this);
---
+++ src/cubegame.coffee
@@ -0,0 +1,340 @@
+this.canvas = ''
+this.cell_width = 40
+this.cell_height = 40
+this.height = 600
+this.width = 800
+
+self = this
+
+$ ->
+    self.canvas = document.getElementById('canvas').getContext('2d')
+    init()
+
+
+init=->
+    self.cells = {}
+    self.shapes = []
+    ###
+    self.shapes=[new Cross(80,80),
+                new Cube(200,80),
+                new Cross(320,80),
+                new Stick(80,240)]
+    ###
+    $('#canvas').click (event)->
+            event.preventDefault()
+            
+    $('body').keydown handle_key_event
+
+#    $('body').keypress handle_key_event
+
+    $('body').click handle_click_event
+    
+    generate_brick()
+
+check_rows = ->false
+
+generate_brick=->
+    shape_classes=[Cross,Cube,Stick,ZShape]
+    shape_class = shape_classes[Math.floor(Math.random()*shape_classes.length)]
+    shape = new shape_class(400,40)
+    self.selected_shape = shape
+    shape.fall()
+
+
+clear_cell=(x,y)->
+    delete self.cells[x][y]
+
+get_cell = (x,y)->
+    if not self.cells[x]
+        return undefined
+    return self.cells[x][y]
+
+set_cell=(x,y,cell)->
+    if not self.cells[x]
+        self.cells[x] = {}
+    self.cells[x][y] = cell
+
+handle_key_event=(event)->
+            if not self.selected_shape
+                return
+            direction = direction_from_event(event)
+            if direction
+                selected_shape.move(direction)
+                return
+
+            rotation = event.keyCode == 32
+            if rotation
+                selected_shape.rotate()
+                return
+
+            falling = event.keyCode == 70
+            if falling
+                selected_shape.fall()
+                return
+
+handle_click_event=(event)->
+            event.preventDefault()
+            for shape in self.shapes
+                if shape.point_in_shape(event.offsetX,event.offsetY)
+                    self.selected_shape = shape
+                    return
+
+direction_from_event=(event)->
+        key_map =
+#                38:'up'
+                39:'right'
+                37:'left'
+                40:'down'
+        return key_map[event.keyCode]
+
+random_cells=->
+    for i in [0..20]
+            for j in [0..15]
+                draw_cell(i+j,i*self.cell_width,j*self.cell_height)
+
+random_color=->
+        red = Math.ceil(Math.random()*255)
+        green = Math.ceil(Math.random()*255)
+        blue = Math.ceil(Math.random()*255)
+        if red>255
+                red = 255
+        if green>255
+                green = 255
+        if blue>255
+                blue = 255
+
+        return "rgb(#{red},#{green},#{blue})"
+
+draw_cell=(left_x,top_y)->
+                self.canvas.fillRect(left_x,top_y,self.cell_width,self.cell_height)
+
+class Figure
+
+        color:'rgb(255,255,255)'
+        cells:[]
+        @width = 0
+        @height = 0
+        @x = 0
+        @y = 0
+
+        constructor:(x,y)->
+                @color=random_color()
+                @cells = []
+                if x? and y?
+                    @draw(x,y)
+
+        point_in_shape:(x,y)->
+                return x>@x and x<@x+@width and y>@y and y<@y+@height
+
+        rotate:->false
+
+        draw:(left_x,top_y)->
+                @x=left_x
+                @y=top_y
+                self.canvas.fillStyle=@color
+
+        outline:->
+                stroke_style = self.canvas.strokeStyle
+                self.canvas.strokeStyle = 'rgb(0,0,0)'
+                self.canvas.strokeRect(@x,@y,@width,@height)
+                self.canvas.strokeStyle = stroke_style
+
+        remove_outline:->
+                stroke_style = self.canvas.strokeStyle
+                self.canvas.strokeStyle = 'rgb(255,255,255)'
+                self.canvas.strokeRect(@x,@y,@width,@height)
+                self.canvas.strokeStyle = stroke_style
+
+        clear:->
+                for cell in @cells
+                    cell.clear()
+                
+
+
+        move:(direction)->
+                if @detect_collision(direction)
+                    return false
+                @clear()
+                if direction == 'up'
+                        @y-=self.cell_height
+                if direction == 'down'
+                        @y+=self.cell_height
+                if direction == 'left'
+                        @x-=self.cell_width
+                if direction == 'right'
+                        @x+=self.cell_width
+                @draw(@x,@y)
+                return true
+
+        detect_collision:(direction)=>
+                if direction == 'up'
+                        for cell in @upper_cells()
+                            if get_cell(cell.x,cell.y-self.cell_height)
+                                return true
+                if direction == 'down'
+                        for cell in @bottom_cells()
+                            if get_cell(cell.x,cell.y+self.cell_height) or @bottom_y()+self.cell_height>self.height
+                                return true
+                if direction == 'left'
+                        for cell in @left_cells()
+                            if get_cell(cell.x-self.cell_width,cell.y)
+                                return true
+                if direction == 'right'
+                        for cell in @right_cells()
+                            if get_cell(cell.x+self.cell_width,cell.y)
+                                return true
+                return false
+        bottom_y:->@y+@height
+
+        fall:=>
+            if @move('down')
+               setTimeout @fall,1000
+            else
+                check_rows()
+                generate_brick()
+
+class Cell extends Figure
+        width:self.cell_width
+        height:self.cell_height
+        
+        constructor:(x,y,color)->
+                @color=color
+                if x? and y?
+                    @draw(x,y)
+        
+        draw:(left_x,top_y)->
+            Cross.__super__.draw.call(this,left_x,top_y)
+            draw_cell(left_x+self.cell_width,top_y)
+            set_cell(left_x,top_y,this)
+
+        clear:()->
+            original_color = @color
+            @color = 'rgb(255,255,255)'
+            @draw(@x,@y)
+            @color = original_color
+            clear_cell(@x,@y)
+
+        
+
+
+class Cross extends Figure
+        width: 3*self.cell_width
+        height:3*self.cell_height
+
+        upper_cells:->[@cells[0],@cells[1],@cells[3]]
+        bottom_cells:->[@cells[1],@cells[4],@cells[3]]
+        left_cells:->[@cells[0],@cells[1],@cells[4]]
+        right_cells:->[@cells[0],@cells[3],@cells[4]]
+
+
+        draw:(left_x,top_y)->
+            Cross.__super__.draw.call(this,left_x,top_y)
+            @cells=[
+                new Cell(left_x+self.cell_width,top_y,@color),
+                new Cell(left_x,top_y+self.cell_height,@color),
+                new Cell(left_x+self.cell_width,top_y+self.cell_height,@color),
+                new Cell(left_x+2*self.cell_width,top_y+cell_height,@color),
+                new Cell(left_x+self.cell_width,top_y+2*cell_height,@color),
+            ]
+
+
+class Cube extends Figure
+        width: 2*self.cell_width
+        height:2*self.cell_height
+
+        upper_cells:->[@cells[0],@cells[1]]
+        bottom_cells:->[@cells[2],@cells[3]]
+        left_cells:->[@cells[0],@cells[2]]
+        right_cells:->[@cells[1],@cells[3]]
+
+        draw:(left_x,top_y)->
+                Cube.__super__.draw.call(this,left_x,top_y)
+                @cells =[
+                    new Cell(left_x,top_y,@color),
+                    new Cell(left_x+self.cell_width,top_y,@color),
+                    new Cell(left_x,top_y+self.cell_height,@color),
+                    new Cell(left_x+self.cell_width,top_y+self.cell_height,@color)
+                ]
+
+class Stick extends Figure
+
+        width: 4*self.cell_width
+        height: self.cell_height
+
+        upper_cells:->
+            if @width>@height
+                @cells
+            else
+                [@cells[0]]
+        bottom_cells:->
+            if @width>@height
+                @cells
+            else
+                [@cells[3]]
+        left_cells:->
+            if @width>@height
+                [@cells[0]]
+            else
+                @cells
+
+        right_cells:->
+            if @width>@height
+                [@cells[3]]
+            else
+                @cells
+
+        rotate:->
+            @clear()
+
+            width = @width
+            height = @height
+
+            @height = width
+            @width = height
+
+            @draw(@x,@y)
+
+        draw:(left_x,top_y)->
+            Stick.__super__.draw.call(this,left_x,top_y)
+
+            if @width>@height
+                #horizontal stick
+                inc=(x,y)-> [x+self.cell_width,y]
+            else
+                #vertical_stick
+                inc=(x,y)-> [x,y+self.cell_height]
+
+            for i in [0..3]
+                if not @cells[i]
+                    @cells[i] = new Cell(null,null,@color)
+                @cells[i].draw(left_x,top_y,@color)
+                result = inc(left_x,top_y)
+                left_x = result[0]
+                top_y = result[1]
+
+class ZShape extends Figure
+    width: 2*self.cell_width
+    height: 2*self.cell_height
+
+    rotated:false
+
+    upper_cells:->[@cells[0],@cells[1],@cells[3]]
+    bottom_cells:->[@cells[0],@cells[2],@cells[3]]
+    left_cells:->[@cells[0],@cells[2]]
+    right_cells:->[@cells[1],@cells[3]]
+       
+    rotate:->
+         
+
+    draw:(left_x,top_y)->
+        ZShape.__super__.draw.call(this,left_x,top_y)
+        @cells =[
+                new Cell(left_x,top_y,@color),
+                new Cell(left_x+self.cell_width,top_y,@color),
+                new Cell(left_x+self.cell_width,top_y+self.cell_height,@color),
+                new Cell(left_x+2*self.cell_width,top_y+self.cell_height,@color)
+            ]
+        
+
+
+
